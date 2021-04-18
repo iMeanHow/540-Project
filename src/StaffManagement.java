d322a54f1f5c0e4348012c08dfadd6175049fc12
mliu25 <mliu25@ncsu.edu>
2021-04-18 23:50:55 +0800

    +search for staff

--- src/StaffManagement.java
+++ src/StaffManagement.java
@@ -1,6 +1,10 @@
+import com.mysql.cj.util.StringUtils;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 import java.util.Scanner;
 
 public class StaffManagement {
@@ -17,6 +21,57 @@ public class StaffManagement {
         this.scanner=scanner;
     }
 
+    public void findStaff(){
+        String sql = "select * from staff where 1=1";
+        System.out.println("--press enter to skip the input--");
+        System.out.print("staffId: ");
+        String x=scanner.nextLine();
+        String id=scanner.nextLine();
+        if(!StringUtils.isNullOrEmpty(id)){
+            sql+=(" and StaffID="+id);
+        }
+        else{
+            System.out.print("name: ");
+            String name=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(name)){
+                sql+=(" and Name='"+name+"'");
+            }
+            System.out.print("age: ");
+            String age=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(age)){
+                sql+=(" and Age="+age);
+            }
+            System.out.print("address: ");
+            String address=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(address)){
+                sql+=(" and Address='"+address+"'");
+            }
+            System.out.print("phone: ");
+            String phone=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(phone)){
+                sql+=(" and Phone='"+phone+"'");
+            }
+        }
+        try {
+            //System.out.println(sql);
+            result = statement.executeQuery(sql);
+            int cnt=0;
+            while (result.next()) {
+                cnt++;
+                System.out.println("\n=== No."+cnt+" ===");
+                System.out.println("staffid: "+result.getInt("StaffID"));
+                System.out.println("name: "+result.getString("Name"));
+                System.out.println("age: "+result.getInt("Age"));
+                System.out.println("address: "+result.getString("Address"));
+                System.out.println("phone: "+result.getString("Phone"));
+                System.out.println("employmentTime: "+result.getDate("EmploymentTime").toString()+"\n");
+            }
+            System.out.println("Total rows: "+cnt);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void createStaff(){
 
     }
@@ -29,6 +84,7 @@ public class StaffManagement {
                 return;
             } else if (in.equals("0")) {
                 System.out.println("--search staff--");
+                findStaff();
             } else if (in.equals("1")) {
                 System.out.println("--new staff--");
             } else if (in.equals("2")) {
