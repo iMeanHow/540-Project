944c1014ee7601a1970d88acdeb0aa561f69d711
mliu25 <mliu25@ncsu.edu>
2021-04-19 16:42:45 +0800

    。

--- src/MemberManagement.java
+++ src/MemberManagement.java
@@ -1,2 +1,127 @@
+
+import com.mysql.cj.util.StringUtils;
+import javax.transaction.*;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+
 public class MemberManagement {
+
+    private Connection connection;
+    private Statement statement;
+    private ResultSet result;
+    private Scanner scanner;
+
+    public MemberManagement(Connection connection,Statement statement,ResultSet result,Scanner scanner){
+        this.connection=connection;
+        this.statement=statement;
+        this.result=result;
+        this.scanner=scanner;
+    }
+
+    public void signUpMember(){
+        
+
+    }
+
+    public void updateMember(){
+
+    }
+
+    public void searchMember(){
+        System.out.println("-----find staff by condition-----");
+        System.out.println("--press enter to skip the input--");
+        String sql = "select * from Store where 1=1";
+        System.out.print("customer id: ");
+        String unuse=scanner.nextLine();
+        String customerid=scanner.nextLine();
+        if(!StringUtils.isNullOrEmpty(customerid)){
+            sql+=(" and CustomerID="+customerid);
+        } else {
+            System.out.print("first name: ");
+            String fname=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(fname)){
+                sql+=(" and FirstName='"+fname+"'");
+            }
+            System.out.print("last name: ");
+            String lname=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(lname)){
+                sql+=(" and LastName='"+lname+"'");
+            }
+            System.out.print("phone: ");
+            String phone=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(phone)){
+                sql+=(" and Phone='"+phone+"'");
+            }
+            System.out.print("email: ");
+            String email=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(email)){
+                sql+=(" and Email='"+email+"'");
+            }
+            System.out.print("address: ");
+            String address=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(address)){
+                sql+=(" and Address like'%"+address+"%'");
+            }
+            System.out.print("level: ");
+            String level=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(level)){
+                sql+=(" and Level='"+level+"'");
+            }
+            System.out.print("activate status: ");
+            String status=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(status)){
+                sql+=(" and ActivateStatus="+status);
+            }
+        }
+        try {
+            //System.out.println(sql);
+            result = statement.executeQuery(sql);
+            int cnt=0;
+            while (result.next()) {
+                cnt++;
+                System.out.println("\n=== No."+cnt+" ===");
+                System.out.println("customerId: "+result.getInt("CustomerID"));
+                System.out.println("first name: "+result.getString("FirstName"));
+                System.out.println("last name: "+result.getString("LastName"));
+                System.out.println("address: "+result.getString("Address"));
+                System.out.println("phone: "+result.getString("Phone"));
+                System.out.println("email: "+result.getString("email"));
+                System.out.println("level: "+result.getString("email"));
+                System.out.println("status (0:inactivate, 1: activate): "+result.getInt("ActivateStatus"));
+            }
+            System.out.println("Total rows: "+cnt);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void execute(){
+        while(true) {
+            System.out.print("enter operation code: ");
+            String in = scanner.next();
+            if (in.equals("back")) {
+                return;
+            } else if(in.equals("0")){
+                System.out.println("--search club member--");
+                searchMember();
+            } else if(in.equals("1")){
+                System.out.println("--sign up club member--");
+                signUpMember();
+            } else if(in.equals("2")){
+                System.out.println("--update club member--");
+                updateMember();
+            } else if(in.equals("3")){
+                System.out.println("--inactivate club member--");
+                updateMember();
+            }
+            else{
+                System.out.println("invalid input");
+            }
+        }
+    }
 }
--- src/SupplierManagement.java
+++ src/SupplierManagement.java
@@ -1,2 +1,190 @@
+import com.mysql.cj.util.StringUtils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.Scanner;
+
 public class SupplierManagement {
+    private Connection connection;
+    private Statement statement;
+    private ResultSet result;
+    private Scanner scanner;
+
+    public SupplierManagement(Connection connection,Statement statement,ResultSet result,Scanner scanner){
+        this.connection=connection;
+        this.statement=statement;
+        this.result=result;
+        this.scanner=scanner;
+    }
+
+    public void findSupplier(){
+        System.out.println("-----find supplier by condition-----");
+        System.out.println("--press enter to skip the input--");
+        String sql = "select * from Suppliers where 1=1";
+        System.out.print("SupplierId: ");
+        //String unuse=scanner.nextLine();
+        String id=scanner.nextLine();
+        if(!StringUtils.isNullOrEmpty(id)){
+            sql+=(" and SupplierID="+id);
+        }
+        else{
+            System.out.print("SupplierName: ");
+            String name=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(name)){
+                sql+=(" and SupplierName='"+name+"'");
+            }
+            System.out.print("Email: ");
+            String email=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(email)){
+                sql+=(" and Email="+email);
+            }
+            System.out.print("Location: ");
+            String Location=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(Location)){
+                sql+=(" and Location='"+Location+"'");
+            }
+            System.out.print("Phone: ");
+            String phone=scanner.nextLine();
+            if(!StringUtils.isNullOrEmpty(phone)){
+                sql+=(" and Phone='"+phone+"'");
+            }
+        }
+        try {
+            //System.out.println(sql);
+            result = statement.executeQuery(sql);
+            int cnt=0;
+            while (result.next()) {
+                cnt++;
+                System.out.println("\n=== No."+cnt+" ===");
+                System.out.println("Supplierid: "+result.getInt("SupplierID"));
+                System.out.println("Name: "+result.getString("Name"));
+                System.out.println("Email: "+result.getString("Email"));
+                System.out.println("Location: "+result.getString("Location"));
+                System.out.println("phone: "+result.getString("Phone")+"\n");
+            }
+            System.out.println("Total rows: "+cnt);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void createSupplier(){
+        System.out.println("--------create new supplier---------");
+        System.out.println("-----no null value permitted-----");
+        String sql = "Insert into supplier (SupplierName,Email,Location,Phone) values (";
+        System.out.print("name: ");
+        sql += ("'"+scanner.next()+"'"+",");
+        System.out.print("email: ");
+        sql += scanner.next()+",";
+        System.out.print("location: ");
+        sql += ("'"+scanner.next()+"'"+",");
+        System.out.print("phone: ");
+        sql += ("'"+scanner.next()+"'"+",");
+
+        try {
+            //System.out.println(sql);
+            int res = statement.executeUpdate(sql);
+            System.out.println("Success");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void deleteSupplier(){
+        System.out.println("--delete Supplier (not recommended)--");
+        System.out.print("Supplier id: ");
+        String id=scanner.next();
+        int Supplierid;
+        try{
+            Supplierid=Integer.valueOf(id);
+        }catch (Exception e){
+            System.out.println("invalid input!");
+        }
+        String sql = "DELETE from Supplier where SupplierID = ("+id+")";
+        try{
+            int res = statement.executeUpdate(sql);
+            System.out.println("Success");
+        } catch (SQLException e) {
+            System.out.println("Supplier Not Exist");
+        }
+    }
+
+    public void updateSupplier(){
+        System.out.println("-----update Supplier-----");
+        System.out.print("Supplier id: ");
+        String id=scanner.next();
+        int Supplierid;
+        try{
+            Supplierid=Integer.valueOf(id);
+        }catch (Exception e){
+            System.out.println("invalid input!");
+        }
+        String sql1 = "select * from Supplier where SupplierID = ("+id+")";
+        try{
+            result = statement.executeQuery(sql1);
+            if(result.next()){
+                System.out.println("Supplierid: "+result.getInt("SupplierID"));
+                System.out.println("Name: "+result.getString("Name"));
+                System.out.println("Email: "+result.getString("Email"));
+                System.out.println("Location: "+result.getString("Location"));
+                System.out.println("phone: "+result.getString("Phone")+"\n");
+            } else {
+                System.out.println("Supplier Not Exist");
+                return;
+            }
+        } catch (SQLException e) {
+            System.out.println("Supplier Not Exist");
+        }
+        String sql2 = "Update Supplier set ";
+        System.out.print("name: ");
+        String name=scanner.next();
+        sql2+=(" Name='"+name+"'");
+        System.out.print("email: ");
+        String email=scanner.next();
+        sql2+=(" ,email="+email);
+        System.out.print("location: ");
+        String location=scanner.next();
+        sql2+=(" ,location='"+location+"'");
+        System.out.print("phone: ");
+        String phone=scanner.next();
+        sql2+=(" ,Phone='"+phone+"'");
+
+        sql2+=(" where SupplierID="+id);
+        try{
+            int res = statement.executeUpdate(sql2);
+            System.out.println("success");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void execute(){
+        while(true) {
+            System.out.print("enter operation code: ");
+            String in = scanner.next();
+            if (in.equals("back")) {
+                return;
+            } else if (in.equals("0")) {
+                System.out.println("--search supplier--");
+                findSupplier();
+            } else if (in.equals("1")) {
+                System.out.println("--new supplier--");
+                createSupplier();
+            }
+            else if (in.equals("2")) {
+                System.out.println("--delete supplier--");
+                deleteSupplier();
+            }
+            else if (in.equals("3")) {
+                System.out.println("--update supplier info by id--");
+                updateSupplier();
+            }
+            else{
+                System.out.println("invalid input");
+            }
+        }
+    }
 }
